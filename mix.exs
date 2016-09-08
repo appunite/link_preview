fcc008e73ef4f311167282ba1d2be0e95d2f55cc
Tobiasz <tobiasz.malecki@appunite.com>
2016-09-08 10:09:48 +0200

    updated mix config and gitignore

--- mix.exs
+++ mix.exs
@@ -2,31 +2,35 @@ defmodule LinkPreviewGenerator.Mixfile do
   use Mix.Project
 
   def project do
-    [app: :link_preview_generator,
-     version: "0.0.1",
-     elixir: "~> 1.2",
-     build_embedded: Mix.env == :prod,
-     start_permanent: Mix.env == :prod,
-     deps: deps]
+    [
+      app: :link_preview_generator,
+      version: "0.0.1",
+      elixir: "~> 1.3",
+      build_embedded: Mix.env == :prod,
+      start_permanent: Mix.env == :prod,
+      deps: deps,
+      aliases: aliases
+    ]
   end
 
-  # Configuration for the OTP application
-  #
-  # Type "mix help compile.app" for more information
   def application do
-    [applications: [:logger]]
+    [
+      applications: [:logger, :httpoison, :floki]
+    ]
   end
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:mydep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
-  #
-  # Type "mix help deps" for more examples and options
   defp deps do
-    []
+    [
+      {:ex_doc, "~> 0.12", only: :dev},
+      {:floki, "~>0.8 and <0.10.0"},
+      {:httpoison, "~>0.9"},
+      {:mock, "~>0.1", only: :test}
+    ]
+  end
+
+  defp aliases do
+    [
+      "test": ["test --exclude excluded"]
+    ]
   end
 end

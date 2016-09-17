5fa85c0635a7ed6f1bf6bf1b5e9f09a1fc8c0400
Karol Wojtaszek <karol@appunite.com>
2016-09-17 11:26:20 +0200

    first package

--- mix.exs
+++ mix.exs
@@ -8,9 +8,27 @@ defmodule LinkPreviewGenerator.Mixfile do
       elixir: "~> 1.3",
       build_embedded: Mix.env == :prod,
       start_permanent: Mix.env == :prod,
+      description: description,
+      package: package,
       deps: deps,
       aliases: aliases
     ]
+  end
+
+  defp description do
+    """
+    LinkPreviewGenerator is a package that tries to receive meta information from given http(s) address
+    """
+  end
+
+  defp package do
+    [
+      files: ["lib", "config", "mix.exs", "README.md"],
+      maintainers: ["Tobiasz Małecki", "Karol Wojtaszek"],
+      licenses: ["Apache 2.0"],
+      links: %{"GitHub" => "https://github.com/appunite/link_preview_generator",
+            "Docs" => "http://hexdocs.pm/link_preview_generator/"}
+   ]
   end
 
   def application do

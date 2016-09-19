7013da874c9ba8836ab4a1706f45cb70f0856af3
Karol Wojtaszek <karol@appunite.com>
2016-09-19 10:57:09 +0200

    version bump

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule LinkPreviewGenerator.Mixfile do
   def project do
     [
       app: :link_preview_generator,
-      version: "0.0.2",
+      version: "0.0.3",
       elixir: "~> 1.3",
       build_embedded: Mix.env == :prod,
       start_permanent: Mix.env == :prod,
